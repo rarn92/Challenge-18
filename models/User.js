79a9b4514ebaa79e0c0e1f44b5f5e9bac0abbb46
Robert Arn <106767290+rarn92@users.noreply.github.com>
2022-08-23 02:26:29 -0500

    commit
    first commit

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      // add email match
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
---
+++ models/index.js
@@ -0,0 +1,4 @@
+const User = require('./User');
+const Course = require('./Thought');
+
+module.exports = { User, Course };
